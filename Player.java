1be4ae39683985e2c4373973bae57f31f912c99b
Корниенко Антон <111607633+AntoNVeK@users.noreply.github.com>
2024-11-24 00:43:28 +0300

    Add files via upload
    Copy 2 files second_branch

---
+++ Player.java
@@ -0,0 +1,12 @@
+public class Player {
+
+    private final char role;
+
+    public Player(char role) {
+        this.role = role;
+    }
+
+    public char getRole() {
+        return this.role;
+    }
+}
